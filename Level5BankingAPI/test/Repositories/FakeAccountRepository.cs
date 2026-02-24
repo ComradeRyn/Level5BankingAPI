4e335fa6259ebf715290483aa14110e1615a8de4
ComradeRyn <ryanyuncza@gmail.com>
2026-02-24 13:17:50 -0500

    Added FakeAccountRepository to test project

--- Level5BankingAPI/test/Repositories/FakeAccountRepository.cs
+++ Level5BankingAPI/test/Repositories/FakeAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using Application.DTOs;
+﻿using System.Linq.Expressions;
+using Application.DTOs;
 using Application.Interfaces;
 using Account = Domain.Models.Account;
 
@@ -6,6 +7,13 @@ namespace test.Repositories;
 
 public class FakeAccountRepository : IAccountsRepository
 {
+    private readonly Dictionary<string, Account> _accounts;
+
+    public FakeAccountRepository(Dictionary<string, Account> accounts)
+    {
+        _accounts = accounts;
+    }
+    
     public Task<(IEnumerable<Account>, PaginationMetadata)> GetAccounts(
         string? name, 
         string? sortBy, 
@@ -13,21 +21,68 @@ public class FakeAccountRepository : IAccountsRepository
         int pageNumber, 
         int pageSize)
     {
-        throw new NotImplementedException();
+        var query = _accounts.Values.AsQueryable();
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            name = name.Trim();
+            query = query.Where(account => account.HolderName.Contains(name));
+        }
+
+        if (!string.IsNullOrWhiteSpace(sortBy))
+        {
+            Expression<Func<Account, object>> sortPattern = sortBy switch
+            {
+                "name" => account => account.HolderName,
+                "balance" => account => account.Balance,
+                _ => account => account.Id
+            };
+
+            query = isDescending
+                ? query.OrderByDescending(sortPattern)
+                : query.OrderBy(sortPattern);
+        }
+        
+        var itemCount = query.Count();
+        var paginationMetadata = new PaginationMetadata(pageNumber,
+            pageSize,
+            itemCount);
+        
+        var accounts = query.Skip(pageSize * (pageNumber - 1))
+            .Take(pageSize)
+            .ToList();
+
+        return Task.FromResult<(IEnumerable<Account>, PaginationMetadata)>((accounts, paginationMetadata));
     }
 
     public Task<Account?> GetAccount(string id)
     {
-        throw new NotImplementedException();
+        _accounts.TryGetValue(id, out var account);
+        
+        return Task.FromResult(account);
     }
 
     public Task<Account> AddAccount(string name)
     {
-        throw new NotImplementedException();
+        var account = new Account
+        {
+            Id = Guid.NewGuid().ToString(),
+            HolderName = name,
+        };
+        
+        _accounts.Add(account.Id, account);
+
+        return Task.FromResult(account);
     }
 
     public Task<Account> UpdateAccount(Account account, decimal amount)
     {
-        throw new NotImplementedException();
+        // TODO: look and see if this needs to be done
+        var accountFromDictionary = _accounts[account.Id];
+        accountFromDictionary.Balance = amount;
+
+        return Task.FromResult(accountFromDictionary);
     }
+
+    public void AddExistingAccount(Account account)
+        => _accounts.Add(account.Id, account);
 }
