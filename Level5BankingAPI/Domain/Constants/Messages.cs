c8422a79d78edc404d353fcf3c53e007d6d9c3d5
ComradeRyn <ryanyuncza@gmail.com>
2026-02-24 10:51:01 -0500

    Added Models and Constants to the Domain project

---
+++ Level5BankingAPI/Domain/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants;
+
+public static class Messages
+{
+    public const string NotFound = "Requested Account could not be found";
+    public const string InsufficientBalance = "Requested amount must be less or equal to current balance";
+    public const string NoNegativeAmount = "Requested amount must be positive";
+    public const string InvalidName = "Requested name must follow the patter of <First> <Middle> <Last>";
+    public const string InvalidSearchType = "Requested search type must be balance or name";
+}
---
+++ Level5BankingAPI/Domain/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models;
+
+public class Account
+{
+    private int InternalId;
+    public required string Id { get; init; }
+    public required string HolderName { get; init; }
+    public decimal Balance { get; set; }
+}
