92a75f3275721b595280e7add38c82c52c5cedab
ComradeRyn <ryanyuncza@gmail.com>
2026-02-24 14:30:07 -0500

    Added GetAccounts test cases

---
+++ Level5BankingAPI/test/Accounts/CreateTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using Application.DTOs.Requests;
+using Application.Services;
+using Domain.Models;
+using test.Repositories;
+
+namespace test.Accounts;
+
+public class CreateTests
+{
+    [Fact]
+    public async Task Create_AccountWithValidName_ReturnCreatedAccount()
+    {
+        // Arrange
+        const string validName = "Ryan L Yuncza";
+        var service = CreateService();
+        var request = new CreationRequest(validName);
+        
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+        var expectedContent = new Application.DTOs.Account("0",validName,0);
+
+        // Act
+        var actual = await service.CreateAccount(request);
+
+        // Assert
+        Assert.Equal(expectedStatusCode, actual.StatusCode);
+        Assert.Equal(expectedContent, actual.Content);
+    }
+
+    [Fact]
+    public async Task Create_AccountWithInvalidName_ReturnFailure()
+    {
+        // Arrange
+        const string invalidName = "invalid";
+        var service = CreateService();
+        var request = new CreationRequest(invalidName);
+
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.BadRequest;
+        var expectedContent = null as Application.DTOs.Account;
+
+        // Act
+        var actual = await service.CreateAccount(request);
+        
+        // Assert
+        Assert.Equal(expectedStatusCode, actual.StatusCode);
+        Assert.Equal(expectedContent, actual.Content);
+    }
+
+    private static AccountsService CreateService()
+    {
+        var accountsDictionary = new Dictionary<string, Account>();
+        var repository = new FakeAccountRepository(accountsDictionary);
+        
+        return new AccountsService(repository, null!);
+    }
+}
--- Level5BankingAPI/test/Accounts/GetAccountsTests.cs
+++ Level5BankingAPI/test/Accounts/GetAccountsTests.cs
@@ -9,48 +9,64 @@ namespace test.Accounts;
 public class GetAccountsTests
 {
     [Fact]
-    public async Task Create_AccountWithValidName_ReturnCreatedAccount()
+    public async Task Get_NonExistentAccount_ReturnFailure()
     {
         // Arrange
-        const string validName = "Ryan L Yuncza";
+        const string nonIncludedId = "no account";
         var service = CreateService();
-        var request = new CreationRequest(validName);
         
-        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
-        var expectedContent = new Application.DTOs.Account("0",validName,0);
-
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.NotFound;
+        var expectedContent = null as Application.DTOs.Account;
+        
         // Act
-        var actual = await service.CreateAccount(request);
-
+        var actual = await service.GetAccount(nonIncludedId);
+        
         // Assert
         Assert.Equal(expectedStatusCode, actual.StatusCode);
         Assert.Equal(expectedContent, actual.Content);
     }
 
     [Fact]
-    public async Task Create_AccountWithInvalidName_ReturnFailure()
+    public async Task Get_ExistentAccount_ReturnFoundAccount()
     {
         // Arrange
-        const string invalidName = "invalid";
-        var service = CreateService();
-        var request = new CreationRequest(invalidName);
+        var account = new Account
+        {
+            HolderName = "Ryan L Yuncza",
+            Balance = 0,
+            Id = "0"
+        };
+        
+        var repository = CreateRepository();
+        var service = CreateService(repository);
+        repository.AddExistingAccount(account);
 
-        const HttpStatusCode expectedStatusCode = HttpStatusCode.BadRequest;
-        var expectedContent = null as Application.DTOs.Account;
-
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+        var expectedContent = new Application.DTOs.Account(account.Id, account.HolderName, 0);
+        
         // Act
-        var actual = await service.CreateAccount(request);
+        var actual = await service.GetAccount(account.Id);
         
         // Assert
         Assert.Equal(expectedStatusCode, actual.StatusCode);
         Assert.Equal(expectedContent, actual.Content);
     }
-
+    
     private static AccountsService CreateService()
     {
         var accountsDictionary = new Dictionary<string, Account>();
         var repository = new FakeAccountRepository(accountsDictionary);
         
         return new AccountsService(repository, null!);
+    }
+
+    private static AccountsService CreateService(FakeAccountRepository repository)
+        => new AccountsService(repository, null!);
+
+    private static FakeAccountRepository CreateRepository()
+    {
+        var accountsDictionary = new Dictionary<string, Account>();
+        
+        return new FakeAccountRepository(accountsDictionary);
     }
 }
