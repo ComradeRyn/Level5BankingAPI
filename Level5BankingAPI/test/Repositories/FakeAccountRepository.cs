23e01ac221acd799a32387535de427fe10d0b35d
ComradeRyn <ryanyuncza@gmail.com>
2026-02-24 12:01:19 -0500

    Added FakeAccountsRepository to test project

---
+++ Level5BankingAPI/test/Repositories/FakeAccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Account = Domain.Models.Account;
+
+namespace test.Repositories;
+
+public class FakeAccountRepository : IAccountsRepository
+{
+    public Task<(IEnumerable<Account>, PaginationMetadata)> GetAccounts(
+        string? name, 
+        string? sortBy, 
+        bool isDescending, 
+        int pageNumber, 
+        int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account?> GetAccount(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account> AddAccount(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account> UpdateAccount(Account account, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
