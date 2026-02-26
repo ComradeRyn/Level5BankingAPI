705150a5fc297a6f7476dcd0f9ae3d80182bfec8
ComradeRyn <ryanyuncza@gmail.com>
2026-02-26 15:40:24 -0500

    Added test for granting users authentication tokens

---
+++ Level5BankingAPI/test/Authentication/AuthenticationTests.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace Test.Authentication;
+
+public class AuthenticationTests
+{
+    [Fact]
+    public void Create_Token_ReturnToken()
+    {
+        // Arrange
+        var service = new AuthenticationService(BuildConfiguration());
+        
+        // Act
+        var actual = service.CreateToken();
+        
+        // Assert
+        Assert.NotNull(actual.Token);
+    }
+
+    private static IConfiguration BuildConfiguration()
+    {
+        var myConfiguration = new Dictionary<string, string>
+        {
+            {
+                "Authentication:SecretForKey",
+                "VGhpcyBpcyBhIHNlY3JldCBwYXNzd29yZCB3aGljaCB3aWxsIGJlIHVzZWQgdG8gZW5jb2RlIHNlY3JldCB0aGluZ3M="
+            },
+            { "Authentication:Issuer", "http://localhost:5079" },
+            { "Authentication:Audience", "bankingapi" }
+        };
+        
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(myConfiguration!)
+            .Build();
+    }
+}
